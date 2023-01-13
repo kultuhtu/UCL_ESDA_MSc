2d4c0e91b8fe97d5b752250b3a5383a40b032980
Ulvi <47240652+kultuhtu@users.noreply.github.com>
2023-01-13 17:11:24 +0400

    Delete 3.SM/shiny_app directory

--- 3.SM/shiny_app/README.md
+++
@@ -1,1 +0,0 @@
-Code for the shiny online app with an overview of the project
--- 3.SM/shiny_app/plot_household_distribution.R
+++
@@ -1,8 +0,0 @@
-plot_household_distribution <- function(df, input_group) {
-  df <- df %>% filter(Group %in% input_group)
-  PieDonut(df, aes(Group, Tariff), ratioByGroup = FALSE, 
-           labelposition = 1,
-           pieLabelSize = 4,
-           donutLabelSize = 4
-           )
-}
